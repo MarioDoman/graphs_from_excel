2b84b4b6a75da54b21cd56f7b2b59aa438cd62fd
MarioDoman <mario.doman@pantheon.tech>
2022-03-15 12:24:21 +0100

    initial commit
    Change-Id: Ie7806aef0e1232791819157c6102a9a7292d30f9

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://community-open-weather-map.p.rapidapi.com/forecast"
+
+querystring = {"q":"bratislava,sk"}
+
+headers = {
+    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+    'x-rapidapi-key': "5f45b4b2f9msh70a2f9ff2fdaf61p160266jsn303646720889"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
